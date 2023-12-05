ff29b095502e6ef74ad9054616c31265079b4596
Erick Barros <erickbarrosrezende@gmail.com>
2023-12-05 20:37:07 -0300

    feat(User): created user model

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", schema);
+
+module.exports = User;
