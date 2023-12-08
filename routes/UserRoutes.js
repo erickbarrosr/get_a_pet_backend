10d4b7a2ddea7adaa3d8cc6458b31fe5c39b76db
Erick Barros <erickbarrosrezende@gmail.com>
2023-12-08 20:25:15 -0300

    feat(UserRoutes): set routes for login and check user logged in

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const UserController = require("../controllers/UserController");
+const LoginController = require("../controllers/LoginController");
 
 router.post("/register", UserController.register);
+router.post("/login", LoginController.login);
+router.get("/check", LoginController.checkIfUserIsLoggedIn);
 
 module.exports = router;
