11333012177227654cd34c16b90a130a95c9f8a5
Erick Barros <erickbarrosrezende@gmail.com>
2023-12-19 20:35:07 -0300

    feat(UserController): created method to update user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,7 @@
 const User = require("../models/User");
+const getToken = require("../helpers/get-token");
+const getUserByToken = require("../helpers/get-user-by-token");
+const bcrypt = require("bcrypt");
 
 module.exports = class UserController {
   static async registerUser(req, res) {
@@ -50,5 +53,64 @@ module.exports = class UserController {
     }
 
     res.status(200).json({ user });
+  }
+
+  static async editUser(req, res) {
+    try {
+      const { name, email, phone, password, confirmPassword } = req.body;
+
+      const id = req.params.id;
+
+      const token = getToken(req);
+
+      const user = await getUserByToken(token);
+
+      if (!name) {
+        return res.status(422).json({ message: "O nome é obrigatório." });
+      }
+
+      user.name = name;
+
+      if (!email) {
+        return res.status(422).json({ message: "O email é obrigatório." });
+      }
+
+      const userExists = await User.findOne({ email });
+
+      if (user.email !== email && userExists) {
+        return res
+          .status(422)
+          .json({ message: "Por favor, utilize outro e-mail!" });
+      }
+
+      user.email = email;
+
+      if (!phone) {
+        return res.status(422).json({ message: "O telefone é obrigatório." });
+      }
+
+      user.phone = phone;
+
+      if (password != confirmPassword) {
+        return res.status(422).json({ message: "As senhas não conferem!" });
+      } else if (password === confirmPassword && password != null) {
+        const salt = await bcrypt.genSalt(12);
+
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        user.password = passwordHash;
+      }
+
+      await User.findOneAndUpdate(
+        { _id: user._id },
+        { $set: user },
+        { new: true }
+      );
+
+      res.status(200).json({ message: "Usuário atualizado com sucesso!" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Falha ao editar usuário." });
+    }
   }
 };
