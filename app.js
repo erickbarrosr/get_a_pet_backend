c0f92d4baf29550fca44d044773bd6ca0c2a4bff
Erick Barros <erickbarrosrezende@gmail.com>
2024-02-01 20:55:57 -0300

    feat(app.js): imported pet route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const db = require("./db");
 const UserRoutes = require("./routes/UserRoutes");
+const PetRoutes = require("./routes/PetRoutes");
 
 const express = require("express");
 const cors = require("cors");
@@ -13,5 +14,6 @@ app.use(express.json());
 app.use(cors({ credentials: true, origin: "http://localhost:4200" }));
 app.use(express.static("public"));
 app.use("/users", UserRoutes);
+app.use("/pets", PetRoutes);
 
 module.exports = app;
