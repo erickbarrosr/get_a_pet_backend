7986c19ab136181172971a1895474994fd5c0f92
Erick Barros <erickbarrosrezende@gmail.com>
2024-02-01 19:48:25 -0300

    feat(UserRoutes.js): created route for to edit users

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,6 +1,7 @@
 const UserController = require("../controllers/UserController");
 const LoginController = require("../controllers/LoginController");
 const verifyToken = require("../helpers/verify-token");
+const { imageUpload } = require("../helpers/image-upload");
 
 const router = require("express").Router();
 
@@ -8,6 +9,11 @@ router.post("/register", UserController.registerUser);
 router.post("/login", LoginController.authenticateUser);
 router.get("/check", LoginController.checkIfUserIsLoggedIn);
 router.get("/:id", UserController.getUserById);
-router.patch("/edit/:id", verifyToken, UserController.editUser);
+router.patch(
+  "/edit/:id",
+  verifyToken,
+  imageUpload.single("image"),
+  UserController.editUser
+);
 
 module.exports = router;
